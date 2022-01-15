d196de20abdc4aca19f60e35dda6493ba47b90a8
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-15 15:38:11 +0900

    [feature/gallery] Retrofit DI Config

---
+++ app/src/main/java/com/l2hyunwoo/phorest/config/di/RetrofitModule.kt
@@ -0,0 +1,41 @@
+package com.l2hyunwoo.phorest.config.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): Interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: Interceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        urlStore: BaseUrlStore,
+        client: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(urlStore.picBaseUrl)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
