f269c97c10eec2cbc1197f8d4b9bf4e38a302957
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-15 12:42:13 +0900

    [feature/gallery] 사진 보여줄 화면 GalleryActivity로 네이밍 교체

---
+++ app/src/main/java/com/l2hyunwoo/phorest/presentation/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.l2hyunwoo.phorest.presentation
+
+import android.os.Bundle
+import com.l2hyunwoo.phorest.R
+import com.l2hyunwoo.phorest.core.base.BindingActivity
+import com.l2hyunwoo.phorest.databinding.ActivityGalleryBinding
+
+class GalleryActivity : BindingActivity<ActivityGalleryBinding>(R.layout.activity_gallery) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
