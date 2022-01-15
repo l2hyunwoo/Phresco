fdfffe52d580d434ea58e79910a8b75e7aaf1f87
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-15 14:04:22 +0900

    [feature/gallery] BottomNavigationView 설정 및 화면에 부착

--- app/src/main/java/com/l2hyunwoo/phorest/presentation/GalleryActivity.kt
+++ app/src/main/java/com/l2hyunwoo/phorest/presentation/GalleryActivity.kt
@@ -8,5 +8,16 @@ import com.l2hyunwoo.phorest.databinding.ActivityGalleryBinding
 class GalleryActivity : BindingActivity<ActivityGalleryBinding>(R.layout.activity_gallery) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.bnvMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_home -> {
+                    true
+                }
+                R.id.page_favorite -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
