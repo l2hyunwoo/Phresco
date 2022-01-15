409cd13208d55b5890ccf70f0a0e825cf9cce445
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-15 11:30:05 +0900

    [feature/theme] Material Design 버전 3로 올리고 theme, color 설정

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val kotlinVersion = "1.5.31"
     const val coreKtxVersion = "1.7.0"
     const val appCompatVersion = "1.4.0"
-    const val materialDesignVersion = "1.4.0"
+    const val materialDesignVersion = "1.5.0"
     const val constraintLayoutVersion = "2.1.2"
     const val legacySupportVersion = "1.0.0"
     const val hiltVersion = "2.38.1"
